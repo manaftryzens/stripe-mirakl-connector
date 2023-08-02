432742755ea7bbeddff630c44d58806693f8f3b8
manaf <manaf.pm@tryzens.com>
2023-08-02 17:08:46 +0530

    Revert "Fix function deprecation issues bootKernel method"
    This reverts commit 2bd1dc4c661dd551e78dd62387496d3e441f63ce.

--- tests/Command/PaymentRefundCommandTest.php
+++ tests/Command/PaymentRefundCommandTest.php
@@ -57,19 +57,11 @@ class PaymentRefundCommandTest extends KernelTestCase
         private $transferReceiver;
 
         /**
-         * {@inheritdoc}
-         */
-        protected function runBootKernel()
-        {
-            return self::bootKernel();
-        }
-
-        /**
          * @return void
          */
         protected function setUp(): void
         {
-                $kernel = $this->runBootKernel();
+                $kernel = self::bootKernel();
                 $application = new Application($kernel);
                 $this->command = $application->find('connector:dispatch:process-refund');
                 $this->commandTester = new CommandTester($this->command);
